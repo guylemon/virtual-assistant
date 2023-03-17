6a40af446159e778ac0d1a6cb308015ec7e33865
Me <me@eci>
2023-03-17 18:42:36 -0500

    chore: change tabs to spaces.

--- main.sh
+++ main.sh
@@ -4,49 +4,49 @@ source .env
 
 # Use this function when receiving input from a pipe.
 function read_stdin() {
-	# -r treats backslash as part of the line to preserve newlines.
-	while read -r line; do
-		input="${input}${line}"
-	done
+  # -r treats backslash as part of the line to preserve newlines.
+  while read -r line; do
+    input="${input}${line}"
+  done
 
-	# don't add a trailing new line to the output
-	echo -n ${input}
+  # don't add a trailing new line to the output
+  echo -n ${input}
 }
 
 
 # Escape text input to support prompts that include JSON text.
 function process_user_input() {
-	read_stdin \
-		| jq --raw-input
+  read_stdin \
+    | jq --raw-input
 }
 
 function get_payload() {
-	local user_message="$(read_stdin)"
+  local user_message="$(read_stdin)"
 
-	cat <<EOF
-	{
-		"model": "gpt-3.5-turbo",
-		"messages": [{"role": "user", "content": ${user_message} }],
-		"top_p": 0.01
-	}
+  cat <<EOF
+  {
+    "model": "gpt-3.5-turbo",
+    "messages": [{"role": "user", "content": ${user_message} }],
+    "top_p": 0.01
+  }
 EOF
 }
 
 
 function get_openai_chat_completion() {
-	curl \
-		--silent \
-		https://api.openai.com/v1/chat/completions \
-			-H 'Content-Type: application/json' \
-			-H "Authorization: Bearer ${API_KEY}" \
-			-d "$(read_stdin)"
+  curl \
+    --silent \
+    https://api.openai.com/v1/chat/completions \
+      -H 'Content-Type: application/json' \
+      -H "Authorization: Bearer ${API_KEY}" \
+      -d "$(read_stdin)"
 }
 
 function parse_response() {
-	read_stdin \
-		| jq \
-			--raw-output \
-			'.choices[0].message.content'
+  read_stdin \
+    | jq \
+      --raw-output \
+      '.choices[0].message.content'
 }
 
 function print_welcome_message() {
@@ -72,24 +72,24 @@ fi
 # TODO label ai responses
 while true; do
   # Prompt for input
-	if [ -z "$user_input" ]; then
-		read -p "> " user_input
-		continue
-	fi
+  if [ -z "$user_input" ]; then
+    read -p "> " user_input
+    continue
+  fi
 
   # Detect user commands
-	if [ "$user_input" = "/q" ]; then
-		echo "Exiting chat."
-		exit 0
-	fi
+  if [ "$user_input" = "/q" ]; then
+    echo "Exiting chat."
+    exit 0
+  fi
 
   # Get chat response
-	echo $user_input \
-		| process_user_input \
-		| get_payload \
-		| get_openai_chat_completion \
-		| parse_response
+  echo $user_input \
+    | process_user_input \
+    | get_payload \
+    | get_openai_chat_completion \
+    | parse_response
 
-	# Clear user input for next round.
-	unset user_input
+  # Clear user input for next round.
+  unset user_input
 done
